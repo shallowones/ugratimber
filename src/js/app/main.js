b828aae9453da9a5fce802ee1fc2e62920e757b6
bpa <bpa@ugraweb.ru>
2017-10-31 18:34:09 +0500

    Реализованы все страницы проекта

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -34,7 +34,7 @@
       mainSliderThumbs.params.control = mainSlider;
     }
 
-    // инициализация слайдера на главной
+    // инициализация слайдера на детальных
     {
       const countSliders = 4
 
@@ -68,19 +68,6 @@
       $('#lang').selectmenu()
     }
 
-    // фокус инпутов
-    {
-      $('.form__input').on('keyup', (e) => {
-        const $this = $(e.currentTarget)
-        const textClass = 'text'
-        if ($this.val().length > 0) {
-          $this.addClass(textClass)
-        } else {
-          $this.removeClass(textClass)
-        }
-      })
-    }
-
     // инициализация слайдера на детальных страницах
     {
       $('.gallery').swiper({
@@ -88,30 +75,98 @@
       })
     }
 
-    // всплывающие окна
+    // всплывающие окна + фокус инпутов + кастом input[type="file"]
     {
+      const inputFormSelector = '.form__input'
+      const inputFocus = (() => {
+        return (e) => {
+          const $this = $(e.currentTarget)
+          const textClass = 'text'
+          if ($this.val().length > 0) {
+            $this.addClass(textClass)
+          } else {
+            $this.removeClass(textClass)
+          }
+        }
+      })()
+      $(inputFormSelector).on('keyup', inputFocus)
+
+      const inputFileSelector = '.js-file input[type="file"]'
+      const fileCustom = (() => {
+        return ($el) => {
+          $el.jfilestyle({
+            buttonText: 'Обзор',
+            placeholder: '',
+            inputSize: 'calc(100% - 134px)'
+          })
+        }
+      })()
+      fileCustom( $(inputFileSelector) )
+
       const popupSelector = '.js-popup'
       new jBox('Modal', {
         attach: popupSelector,
         closeOnClick: false,
         onOpen: function () {
-          const html = this.source.find('.popup-block').html()
+          const $source = this.source
+          const $content = this.content
+          const $popup = $source.find('.popup-block')
+          $source.find(inputFileSelector).jfilestyle('destroy')
+          const html = $popup.html()
           if (typeof html !== 'undefined' && html.length) {
-            this
-              .setContent(html)
-              .content.find('.popup__button').on('click', (e) => {
+            $popup.remove()
+            this.setContent(html)
+            $content.find('.popup__button').on('click', (e) => {
               e.preventDefault()
               this.close()
             })
+            $content.find(inputFormSelector).on('keyup', inputFocus)
+            fileCustom( $content.find(inputFileSelector) )
           } else {
             this.close()
           }
         },
-        onClose: function () {
-          this.content.find('.popup__button').off('click')
+        onCloseComplete: function () {
+          const $content = this.content
+          $content.find(inputFileSelector).jfilestyle('destroy')
+          const html = $content.html()
+          $content.find('.popup').remove()
+          this.source.append('<div class="popup-block">' + html + '</div>')
         }
       })
       $(popupSelector).on('click', (e) => { e.preventDefault() })
+    }
+
+    // работа с точками на карте
+    {
+      new jBox('Tooltip', {
+        attach: '.js-touch',
+        trigger: 'click',
+        position: {
+          x: 'left',
+          y: 'top'
+        },
+        outside: 'x',
+        closeOnClick: 'body',
+        onOpen: function () {
+          const html = $(this.source.data('target')).html()
+          if (typeof html !== 'undefined' && html.length) {
+            this.setContent(html)
+          } else {
+            this.close()
+          }
+        }
+      });
+    }
+
+    // поиск
+    {
+      $('.js-search').on('click', (e) => {
+        e.preventDefault()
+        const $target = $(e.currentTarget.dataset.target)
+        $target.toggleClass('show')
+        $(e.currentTarget).toggleClass('active')
+      })
     }
 
   })
